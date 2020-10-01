edc1d4db96ea824e6852a7a57b0ccd979c38f484
alipazokit <46820262+alipazokit@users.noreply.github.com>
2020-10-01 01:07:06 -0700

    Update test.sh

--- example/test.sh
+++ example/test.sh
@@ -1,8 +1,7 @@
-
-gen=1
-phen=small.pheno.plink
-covar=sample.cov
-annot=sample.annot.txt
+gen=small_git
+phen=small.phen
+covar=small.cov
+annot=annot_git.txt
 
 ../build/RHEmc -g $gen -p $phen -c $covar -k 10 -jn 100   -o test.out.txt -annot $annot
 
